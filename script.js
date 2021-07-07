95f63b8522e3c6ca0a17472f37ce5ce810cbfbfc
yevhenii-vdovin <45859199+yevhenii-vdovin@users.noreply.github.com>
2021-07-08 00:40:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-const otter = document.getElementById('otter');
-const rock = document.getElementById('rock');
+const dino = document.getElementById('dino');
+const cactus = document.getElementById('cactus');
 
 document.addEventListener('keydown', jump);
 
 function jump() {
-  if (otter.classList != 'jump') {
-    otter.classList.add('jump');
+  if (dino.classList != 'jump') {
+    dino.classList.add('jump');
   }
   setTimeout(function () {
-    otter.classList.remove('jump');
+    dino.classList.remove('jump');
   }, 500);
 }
 
 let active = setInterval(function () {
-  let otterTop = parseInt(
-    window.getComputedStyle(otter).getPropertyValue('top')
-  );
-  console.log(parseInt(window.getComputedStyle(otter).getPropertyValue('top')));
-  let rockLeft = parseInt(
-    window.getComputedStyle(rock).getPropertyValue('left')
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+  console.log(parseInt(window.getComputedStyle(dino).getPropertyValue('top')));
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue('left')
   );
 
-  if (rockLeft < 50 && rockLeft > 0 && otterTop >= 140) {
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
     alert('Game over!');
     return;
   }
